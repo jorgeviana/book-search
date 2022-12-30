0de807ad86542f62239b28e2d95b160cd9909fcf
Jorge Viana <secoviana@gmail.com>
2022-12-30 13:18:44 +0100

    make members of Commands private

--- src/main/kotlin/com/example/booksearch/Commands.kt
+++ src/main/kotlin/com/example/booksearch/Commands.kt
@@ -3,13 +3,16 @@ package com.example.booksearch
 import org.springframework.stereotype.Component
 
 @Component
-class Commands(val bookService: BookService, val console: Console, val readingList: ReadingList) {
+class Commands(
+    private val bookService: BookService,
+    private val console: Console,
+    private val readingList: ReadingList,
+) {
     fun accept(command: String) {
         if (command.startsWith("list")) {
             val booksList = readingList.get()
-            booksList.forEach { book -> console.printLine("$book")}
-        }
-        else if (command.startsWith("add")) {
+            booksList.forEach { book -> console.printLine("$book") }
+        } else if (command.startsWith("add")) {
             val index = command.split(":")[1].trim().toInt()
             val book = bookService.lastSearchResult().get(index - 1)
             readingList.add(book)
