dea983aa219a3ee58d413e4a0c0c616583522b1d
Jorge Viana <secoviana@gmail.com>
2022-12-31 19:55:33 +0100

    move FakeBookService to test folder

--- src/main/kotlin/com/example/booksearch/BookService.kt
+++ src/main/kotlin/com/example/booksearch/BookService.kt
@@ -1,51 +1,16 @@
 package com.example.booksearch
 
 import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.context.annotation.Primary
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 
-private const val MAX_SEARCH_RESULT = 5L
+const val MAX_SEARCH_RESULT = 5L
 
 interface BookService {
     fun search(criteria: String): List<Book>
     fun lastSearchResult(): List<Book>
 }
 
-@Component
-class FakeBookService : BookService {
-
-    private val books = mutableListOf<Book>()
-    private var lastSearchResult = mutableListOf<Book>()
-
-    fun add(book: Book) {
-        books.add(book)
-    }
-
-    override fun search(criteria: String): List<Book> {
-        val searchResult = books.filter { book ->
-            book.tittle.lowercase().contains(criteria.lowercase())
-                    || book.author.lowercase().contains(criteria.lowercase())
-        }
-            .stream()
-            .limit(MAX_SEARCH_RESULT)
-            .toList()
-
-        updateLastSearchResult(searchResult)
-        return searchResult
-    }
-
-    override fun lastSearchResult(): List<Book> {
-        return lastSearchResult.toList()
-    }
-
-    private fun updateLastSearchResult(searchResult: List<Book>) {
-        lastSearchResult = mutableListOf()
-        lastSearchResult.addAll(searchResult)
-    }
-}
-
-@Primary
 @Component
 class GoogleBookService(restTemplateBuilder: RestTemplateBuilder) : BookService {
 
---
+++ src/test/kotlin/com/example/booksearch/FakeBookService.kt
@@ -0,0 +1,33 @@
+package com.example.booksearch
+
+class FakeBookService : BookService {
+
+    private val books = mutableListOf<Book>()
+    private var lastSearchResult = mutableListOf<Book>()
+
+    fun add(book: Book) {
+        books.add(book)
+    }
+
+    override fun search(criteria: String): List<Book> {
+        val searchResult = books.filter { book ->
+            book.tittle.lowercase().contains(criteria.lowercase())
+                    || book.author.lowercase().contains(criteria.lowercase())
+        }
+            .stream()
+            .limit(MAX_SEARCH_RESULT)
+            .toList()
+
+        updateLastSearchResult(searchResult)
+        return searchResult
+    }
+
+    override fun lastSearchResult(): List<Book> {
+        return lastSearchResult.toList()
+    }
+
+    private fun updateLastSearchResult(searchResult: List<Book>) {
+        lastSearchResult = mutableListOf()
+        lastSearchResult.addAll(searchResult)
+    }
+}
